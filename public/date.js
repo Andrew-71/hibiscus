7b09e5e7b5206e5c2d9f82a31924dcc25046815c
Andrew-71 <andrey.nikitin.vladimirovich@gmail.com>
2024-03-18 15:01:09 +0300

    Move JS out of index.html

---
+++ public/date.js
@@ -0,0 +1,23 @@
+// Set today's date
+function updateDate() {
+    let todayDate = new Date()
+    let timeField = document.getElementById("today-date")
+    timeField.innerText = todayDate.toISOString().split('T')[0]
+
+}
+
+// Starts interval to update today's date every hour at 00:00
+function callEveryHour() {
+    setInterval(updateDate, 1000 * 60 * 60);
+}
+
+// Begin above interval
+function beginDateUpdater() {
+    let nextDate = new Date();
+    nextDate.setHours(nextDate.getHours() + 1);
+    nextDate.setMinutes(0);
+    nextDate.setSeconds(0);
+
+    const difference = nextDate - new Date();
+    setTimeout(callEveryHour, difference);
+}
---
+++ public/requests.js
@@ -0,0 +1,65 @@
+async function postData(url = "", data = "") {
+    const response = await fetch(url, {
+        method: "POST",
+        credentials: "same-origin",
+        headers: {
+            "Content-Type": "text/plain",
+        },
+        redirect: "follow",
+        referrerPolicy: "no-referrer",
+        body: data,
+    });
+    if (response.ok) {
+        return response.text();
+    } else {
+        return response.status
+    }
+}
+
+async function getData(url = "", data = "") {
+    const response = await fetch(url, {
+        method: "GET",
+        credentials: "same-origin",
+        redirect: "follow",
+        referrerPolicy: "no-referrer"
+    });
+    if (response.ok) {
+        return response.text();
+    } else {
+        console.log(response.text())
+        return response.status
+        // return "Error"
+    }
+}
+
+function saveLog() {
+    let logField = document.getElementById("log")
+    postData("/api/log", logField.value).then((data) => {
+        if (data !== 500) {
+            logField.value = ""
+        }
+    });
+}
+
+function saveToday() {
+    let logField = document.getElementById("day")
+    postData("/api/today", logField.value).then((data) => {
+        console.log(data);
+    });
+}
+function loadToday() {
+    let dayField = document.getElementById("day")
+    getData("/api/today", dayField.value).then((data) => {
+        if (data === 404) {
+            dayField.value = ""
+        } else if (data === 401) {
+            dayField.enabled = false
+            dayField.value = "Unauthorized"
+        } else if (data === 500) {
+            dayField.enabled = false
+            dayField.value = "Internal server error"
+        } else {
+            dayField.value = data
+        }
+    });
+}
