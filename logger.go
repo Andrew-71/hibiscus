94952b57669684aa27d33e3ec066c78c4463a00c
Andrew-71 <andrey.nikitin.vladimirovich@gmail.com>
2024-03-23 16:17:06 +0300

    Let logger work without file access

--- logger.go
+++ logger.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"io"
-	"log"
 	"log/slog"
 	"os"
 )
@@ -13,11 +12,11 @@ var LogFile = "config/log.txt"
 func LogInit() {
 	f, err := os.OpenFile(LogFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
-		log.Fatalf("error opening log file: %v", err)
+		slog.SetDefault(slog.New(slog.NewTextHandler(os.Stdout, nil))) // Fallback to stdout
+		slog.Error("error opening log file, logging to stdout", "path", LogFile, "error", err)
+		return
 	}
 	// No defer f.Close() because that breaks the MultiWriter
-
 	w := io.MultiWriter(f, os.Stdout)
-
 	slog.SetDefault(slog.New(slog.NewTextHandler(w, nil)))
 }
